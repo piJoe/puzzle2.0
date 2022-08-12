46f1bc5c74dfa9e7b465534a69653a1beb4bfb01
piJoe <raoul@dagalaxy.net>
2022-08-12 22:51:45 +0200

    cleanup

--- src/app.ts
+++
@@ -1,1 +0,0 @@
-import "./lib/puzzle-gen";
--- src/lib/helper.ts
+++
@@ -1,9 +0,0 @@
-import * as THREE from "three";
-
-export function nextPowerOf2(i: number): number {
-  return 1 << Math.ceil(Math.log2(i));
-}
-
-export function toVector2(v: THREE.Vector3): THREE.Vector2 {
-  return new THREE.Vector2(v.x, v.y);
-}
